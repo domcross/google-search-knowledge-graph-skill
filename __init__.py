7dbc4a995314711e8d5feece800bce29aae7bc70
domcross <dkreutz@t-online.de>
2019-10-14 21:14:44 +0200

    initial commit

--- __init__.py
+++ __init__.py
@@ -1,3 +1,6 @@
+import json
+import urllib
+
 from mycroft import MycroftSkill, intent_file_handler
 
 
@@ -5,9 +8,25 @@ class GoogleSearchKnowledgeGraph(MycroftSkill):
     def __init__(self):
         MycroftSkill.__init__(self)
 
-    @intent_file_handler('graph.knowledge.search.google.intent')
+    def initialize(self):
+        self.api_key = self.settings.get("api_key", "")
+
+    @intent_file_handler('search.intent')
     def handle_graph_knowledge_search_google(self, message):
-        self.speak_dialog('graph.knowledge.search.google')
+        if not self.api_key:
+            self.speak_dialog('no.api.key')
+        self.log.info(message.data)
+        api_key = self.api_key
+        query = 'Taylor Swift'
+        service_url = 'https://kgsearch.googleapis.com/v1/entities:search'
+        params = {
+            'query': query,
+            'limit': 1,
+            'indent': True,
+            'key': api_key
+        }
+        url = service_url + '?' + urllib.parse.urlencode(params)
+        response = json.loads(urllib.request.urlopen(url).read())
 
 
 def create_skill():
